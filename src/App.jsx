9cc15729c76c60d98fc70d21f8ad6841f8927cb5
Jay Gandhi <jaygandhi249@gmail.com>
2025-08-15 20:22:41 +1000

    created states and chrome storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [minutes, setMinutes] = useState(0);
+  const [timerDisplay, setTimerDisplay] = useState("00:00");
+  const [isTracking, setIsTracking] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    chrome.storage.local.get(
+      [
+        //Things stored in local storage in chrome.
+        "trackingUrl", //Url being tracked
+        "timeLimit", //Time limit set by user in minutes
+        "startTime", //Start time of the tracking sessions
+        "isTracking", //Boolean to check if tracking is active
+        "elapsedTime", //Elapsed time (how much has been passed)
+        "notes", //Notes added by user
+      ],
+      (data) => {
+        if (data.isTracking) {
+          setUrl(data.trackingUrl);
+          setMinutes(Math.floor(data.timeLimit / (60 * 1000)));
+          setIsTracking(true);
+        }
+      }
+    );
+  }, []);
+
   return (
     <>
-      <h1 className="text-2xl">Hello world</h1>
+      <h1 className="text-2xl">Hello UQ CS</h1>
     </>
   );
 }
